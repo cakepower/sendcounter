1b46d6c46892c524ad67e37e9477d201d052fbb9
cakepower <32028127+cakepower@users.noreply.github.com>
2021-11-25 08:36:01 +0900

    main.blocks, main.ts, main.py 업데이트하기

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-def on_button_pressed_a():
-    radio.send_value("counter", 1)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_ab():
-    radio.send_value("counter", 0)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
-def on_button_pressed_b():
-    radio.send_value("counter", -1)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
 radio.set_group(33)
 
 def on_forever():
-    basic.show_number(input.sound_level())
+    pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,12 +2,13 @@ input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(0)
+    radio.sendNumber(99)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(-1)
 })
 radio.setGroup(33)
+radio.setTransmitPower(7)
 basic.forever(function () {
 	
 })
